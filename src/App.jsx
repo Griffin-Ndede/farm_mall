0c6353cfd7b8ae6011b82c3d82f9e7681a1f79ee
Griffin <omondigriffin4@gmail.com>
2024-09-20 10:52:45 +0200

    creates links to login and sign up pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
 import React from 'react'
 import Home from './components/Home'
-import Navbar from './components/Navbar'
+import Login from './components/Login'
+import Signup from './components/Signup'
+import { BrowserRouter, Route , Routes} from 'react-router-dom'
 
 function App() {
 
   return (
     <>
-     <Home/>
+    <BrowserRouter>
+    <Routes>
+      <Route path="" element={<Home/>}></Route>
+      <Route path="/login" element={<Login/>}></Route>
+      <Route path="/signup" element={<Signup/>}></Route>
+    </Routes>
+    </BrowserRouter>
     </>
   )
 }
