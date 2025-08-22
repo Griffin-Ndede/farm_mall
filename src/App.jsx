8e83a212da0bfb87eddd7a0cdf6eda6f77f4f23f
Griffin <omondigriffin4@gmail.com>
2025-08-22 10:56:32 +0300

    changes the signup component url

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import Home from './Pages/Home'
 import Dashboard from './Pages/Dashboard'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import Signup from './Components/Signup'
+// import Signup from './Components/Signup'
 import Login from './Components/Login'
 import { UserProvider } from './Context/UserContext'
 import ActivitiesForm from './Pages/ActivitiesForm'
@@ -17,7 +17,7 @@ function App() {
         <BrowserRouter>
           <Routes>
             <Route path="/" element={<Home />}></Route>
-            <Route path='/register' element={<Signup />}></Route>
+            {/* <Route path='/register' element={<Signup />}></Route> */}
             <Route path='/login' element={<Login />}></Route>
             <Route path="/dashboard" element={<Dashboard />}>
               <Route path="activities" element={<ActivitiesForm />} />
